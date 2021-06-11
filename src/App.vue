5cd4feea74e67b3dd92c9c3debfc17cda06d9a57
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-11 19:02:31 -0300

    Componente com typescript

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Contador :valorInicial="10" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Contador from "@/components/Contador.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Contador,
   },
 })
 export default class App extends Vue {}
